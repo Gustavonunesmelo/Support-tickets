74a720be3b745ba0a967ee4c0f8bb10917ff882b
Gustavo nunes melo <156378462+Gustavonunesmelo@users.noreply.github.com>
2025-06-12 13:53:41 -0300

    Filtrando pelo status.

--- src/database/database.js
+++ src/database/database.js
@@ -29,8 +29,17 @@ export class Database {
     this.#persist( )
   }
 
-  select(table) {
+  select(table, filters) {
     let data = this.#database[table] ?? []
+
+    if(filters) {
+      data = data.filter(( row ) => {
+        const test = Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
+
     return data
   }
 }
